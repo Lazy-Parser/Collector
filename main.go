61e4b2af6755bf7aa274c688d461c67e42a8b372
Xepobopa <ismartsdn4477@gmail.com>
2025-04-18 02:12:03 +0200

    sdfs

--- main.go
+++ main.go
@@ -3,11 +3,25 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"Collector/internal/mexc"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("🚀 Starting Collector...")
+	if err := godotenv.Load(); err != nil {
+		return "", "", err
+	}
+
+	mexcFutures := os.Getenv("MEXC_FUTURES_WS")
+	mexcSpot := os.Getenv("MEXC_SPOT_WS")
+
+	fmt.Println("mexcFutures")
+	fmt.Println(mexcFutures)
+	fmt.Println("mexcSpot")
+	fmt.Println(mexcSpot)
 	mexc.Run(context.Background())
 }
